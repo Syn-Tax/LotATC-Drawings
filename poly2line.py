e1a27e053c12e661f1c4110b7b4a39f9419b1fdc
oscar morris <twocap06@gmail.com>
2023-09-16 17:23:18 +0100

    added some things and modified some things

--- poly2line.py
+++ poly2line.py
@@ -5,8 +5,9 @@ with open(sys.argv[1], "r") as f:
     input_obj = json.loads(f.read())
     
 for draw in input_obj["drawings"]:
-    draw["type"] = "line"
-    draw["points"].append(draw["points"][0])
+    if draw["type"] == "polygon":
+        draw["type"] = "line"
+        draw["points"].append(draw["points"][0])
     
 with open(sys.argv[2], "w") as f:
     f.write(json.dumps(input_obj))
